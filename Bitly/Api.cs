aa19ace3d595c8ce03830297d6f6c19898b65311
DaemonNikolay <nikolay-eckert@mail.ru>
2018-10-31 10:08:52 +0700

    Create solution for lab Bitly with TDD

---
+++ Bitly/Api.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bitly
+{
+    public class Api
+    {
+        private string Login { get; set; }
+        private string Password { get; set; }
+
+        public Api(string login, string password)
+        {
+            Login = login;
+            Password = password;
+        }
+
+        public async Task<string> LongLinkToSmall(string link)
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ BitlyTests/ApiTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bitly;
+using System.Threading.Tasks;
+
+namespace BitlyTests
+{
+    [TestClass]
+    public class ApiTests
+    {
+        [TestMethod]
+        public async Task LongLinkToSmallAsync()
+        {
+            var api = new Api("login", "password");
+            var expected = await api.LongLinkToSmall("rew");
+
+            Assert.AreEqual(expected: expected, actual: "https://bitly.com");
+        }
+    }
+}
