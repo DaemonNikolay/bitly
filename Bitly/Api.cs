a0456a695916b5cb227cbf21bf13ebe8c5246de6
DaemonNikolay <nikolay-eckert@mail.ru>
2018-11-01 17:44:42 +0700

    Create send query

--- Bitly/Api.cs
+++ Bitly/Api.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,17 +12,31 @@ namespace Bitly
     public class Api
     {
         private string Login { get; set; }
-        private string Password { get; set; }
+        private string ApiKey { get; set; }
 
-        public Api(string login, string password)
+        public Api(string login, string apiKey)
         {
+            if (string.IsNullOrEmpty(login) && string.IsNullOrEmpty(apiKey))
+            {
+                throw new ArgumentException("Login or password is empty", nameof(login));
+            }
+
             Login = login;
-            Password = password;
+            ApiKey = apiKey;
         }
 
         public async Task<string> LongLinkToSmall(string link)
         {
-            return string.Empty;
+            var result = string.Empty;
+            var url = $"http://api.bit.ly/v3/shorten?login={Login}&apiKey={ApiKey}&longUrl={link}&format=json";
+            using (var webClient = new WebClient())
+            {
+                var uri = new Uri(url);
+                var response = await webClient.UploadValuesTaskAsync(uri, new NameValueCollection());
+                result = Encoding.UTF8.GetString(response);
+            }
+
+            return result;
         }
     }
 }
--- BitlyTests/ApiTests.cs
+++ BitlyTests/ApiTests.cs
@@ -1,17 +1,21 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Bitly;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace BitlyTests
 {
     [TestClass]
     public class ApiTests
     {
+        private const string login = "kinderny";
+        private const string apiKey = "R_5bfe3e1a2f944de7b65a3c33f41546b9";
+
         [TestMethod]
         public async Task LongLinkToSmallAsync()
         {
-            var api = new Api("login", "password");
-            var expected = await api.LongLinkToSmall("rew");
+            var api = new Api(login, apiKey);
+            var expected = await api.LongLinkToSmall("http://nikulux.ru");
 
             Assert.AreEqual(expected: expected, actual: "https://bitly.com");
         }
